26dce4fd589eed0176074a9374f6174742090767
冷开俊 <lengkj@travelsky.com.cn>
2021-06-16 09:30:05 +0800

    feat: init

--- packages/button/src/demos/base.tsx
+++ packages/button/src/demos/base.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Button from '@lengkaixin/button';
+import Button from '@md/button';
 
-import '@lengkaixin/button/lib/style/index';
+import '@md/button/lib/style/index';
 
 export default () => <Button>click</Button>;
--- packages/foo/src/demos/base.tsx
+++ packages/foo/src/demos/base.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Foo from '@lengkaixin/foo';
+import Foo from '@md/foo';
 
-import '@lengkaixin/foo/lib/style/index';
+import '@md/foo/lib/style/index';
 
 export default () => <Foo title="First Demo--- " />;
